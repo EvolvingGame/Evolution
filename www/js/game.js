04709bfc1f202dd2a83a03d68c729d994631df4d
Galen Erickson <gkerickson@wisc.edu>
2017-02-18 19:45:20 -0600

    Everything in the same for loop and spaced correctly

--- www/js/game.js
+++ www/js/game.js
@@ -26,34 +26,40 @@ gameState.prototype = {
         // Desired Tile Width
         var tileWidth = Math.trunc(gameWidth / (numWide*3/2));
         var tileHeight = tileWidth  / hexWidth * hexHeight;
-        var numHigh = Math.trunc(gameHeight / tileHeight);        
-
+        var numHigh = Math.trunc(gameHeight / tileHeight)*2-1;        
         for(j = 0; j < numHigh; j++){
             for(i = 0; i < numWide; i++){
-                tile = game.add.sprite(1.5*i*tileWidth,j*tileHeight,'tile');
-                tile.height = tileHeight;
-                tile.width = tileWidth;
-                tile.inputEnabled = true;
-                tile.events.onInputDown.add(clickHandler, this);
-                var nextTile = {tile:tile, x:i, y:j};
+                var nextTile = {tile:null,x:null,y:null};
+                if(j%2==0){
+                    tile = game.add.sprite(1.5*i*tileWidth,(j/2)*tileHeight,'tile');
+                    tile.height = tileHeight;
+                    tile.width = tileWidth;
+                    tile.inputEnabled = true;
+                    tile.events.onInputDown.add(clickHandler, this);
+                    nextTile = {tile:tile, x:i, y:j};
+                }else{
+                    if(i == numWide - 1)
+                        continue;
+                    tile = game.add.sprite((1.5*i+0.75)*tileWidth,(Math.round(j)/2)*tileHeight,'tile');
+                    tile.height = tileHeight;
+                    tile.width = tileWidth;
+                    tile.inputEnabled = true;
+                    tile.events.onInputDown.add(clickHandler, this);
+                    nextTile = {tile:tile, x:i, y:j};
+                }
                 hexagons.push(nextTile);
             }
         }
 
         for(j = 0; j < numHigh-1; j++){
             for(i = 0; i < numWide-1; i++){
-                tile = game.add.sprite((1.5*i+0.75)*tileWidth,(j+0.5)*tileHeight,'tile');
-                tile.height = tileHeight;
-                tile.width = tileWidth;
-                tile.inputEnabled = true;
-                tile.events.onInputDown.add(clickHandler, this);
-                var nextTile = {tile:tile, x:i, y:j};
             }
         }
     },
 
     update: function () {
         if(time++%100 == 0){
+            for(i = 0; i < active.length; i++){}
 
         }
     },
